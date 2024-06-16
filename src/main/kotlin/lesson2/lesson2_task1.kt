5ecc2500bf9d58962dcb7c2a23f8da77a2b82a04
Arthur <dypis123@gmail.com>
2024-06-16 17:38:33 +0400

    KS-2-1 English Class

---
+++ src/main/kotlin/lesson2/lesson2_task1.kt
@@ -0,0 +1,14 @@
+package org.example.lesson2
+
+fun main(){
+    val firstStudentMark = 3f
+    val secondStudentMark = 4f
+    val thirdStudentMark = 3f
+    val fourthStudentMark = 5f
+    var numberOfStudents = 4f
+    var averageMark:Float
+
+    averageMark = ((firstStudentMark+secondStudentMark+thirdStudentMark+fourthStudentMark)/numberOfStudents)
+
+    println(averageMark)
+}
