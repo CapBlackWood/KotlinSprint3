43f0318503f447981a873f6974de429f168d2130
Arthur <dypis123@gmail.com>
2024-06-16 16:47:24 +0400

    KS-1-1 add Variables

---
+++ src/main/kotlin/lesson1/lesson1_task1.kt
@@ -0,0 +1,9 @@
+package org.example.lesson1
+
+fun main(){
+    val numberOfOrders: Int = 75
+    val notificationAfterOrder: String = "Спасибо за покупку"
+
+    println(numberOfOrders)
+    println(notificationAfterOrder)
+}
