8fcdbf2b3f4159addfa45c2db310aee609658ec4
Arthur <dypis123@gmail.com>
2024-06-16 17:09:38 +0400

    KS-1-3 Yuri Gagarin in space

---
+++ src/main/kotlin/lesson1/lesson1_task3.kt
@@ -0,0 +1,14 @@
+package org.example.lesson1
+
+fun main(){
+    val year = 1961
+    var hour = "09"
+    var minute = "07"
+
+    println("Год полета Юрия Гагарина в космос: $year")
+    println("Взлет: $hour:$minute")
+
+    hour = "10"
+    minute = "55"
+    println("Посадка: $hour:$minute")
+}
