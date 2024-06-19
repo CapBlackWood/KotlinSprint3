13e563e74de9600626ded741c203cf115226c9ee
Arthur <dypis123@gmail.com>
2024-06-19 12:37:03 +0400

    KS-4-2 Customs rework

--- src/main/kotlin/lesson4/lesson4_task2.kt
+++ src/main/kotlin/lesson4/lesson4_task2.kt
@@ -1,28 +1,28 @@
 package org.example.lesson4
 
-const val minAverageWeight = 35
-const val maxAverageWeight = 100
-const val maxAverageVolume = 100
+const val Min_Average_Weight = 35
+const val Max_Average_Weight = 100
+const val Max_Average_Volume = 100
 
-fun main(){
+fun main() {
     val firstGoodsWeight = 20
     val firstGoodsVolume = 80
     val secondGoodsWeight = 50
     val secondGoodsVolume = 100
     var isAverage: Boolean
 
-    if (minAverageWeight<firstGoodsWeight&&firstGoodsVolume<=maxAverageWeight&&firstGoodsVolume<maxAverageVolume){
+    if (Min_Average_Weight < firstGoodsWeight && firstGoodsVolume <= Max_Average_Weight && firstGoodsVolume < Max_Average_Volume) {
         isAverage = true
         println("Груз с весом $firstGoodsWeight кг и объемом $firstGoodsVolume л соответствует категории 'Average': $isAverage")
-    }else{
+    } else {
         isAverage = false
         println("Груз с весом $firstGoodsWeight кг и объемом $firstGoodsVolume л соответствует категории 'Average': $isAverage")
     }
 
-    if (minAverageWeight<secondGoodsWeight&&firstGoodsVolume<=maxAverageWeight&&secondGoodsVolume<maxAverageVolume){
+    if (Min_Average_Weight < secondGoodsWeight && firstGoodsVolume <= Max_Average_Weight && secondGoodsVolume < Max_Average_Volume) {
         isAverage = true
         println("Груз с весом $secondGoodsWeight кг и объемом $secondGoodsVolume л соответствует категории 'Average': $isAverage")
-    }else{
+    } else {
         isAverage = false
         println("Груз с весом $secondGoodsWeight кг и объемом $secondGoodsVolume л соответствует категории 'Average': $isAverage")
     }
