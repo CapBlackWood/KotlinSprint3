e2e501ad9bd2cfd40f8e7cc5a7c2cbcd6391832e
Arthur <dypis123@gmail.com>
2024-06-19 12:33:11 +0400

    KS-2-3 Train rework

--- src/main/kotlin/lesson2/lesson2_task3.kt
+++ src/main/kotlin/lesson2/lesson2_task3.kt
@@ -2,21 +2,15 @@ package org.example.lesson2
 
 const val minutesInHour = 60
 
-fun main(){
+fun main() {
     val departureHour = 9
     val departureMinute = 39
     val travelTime = 457
     var arrivalHour: Int
     var arrivalMinute: Int
 
-    arrivalHour = travelTime / minutesInHour + departureHour
-    arrivalMinute = travelTime % minutesInHour +departureMinute
+    arrivalHour = (travelTime + departureMinute) / minutesInHour + departureHour
+    arrivalMinute = (travelTime + departureMinute) % minutesInHour
 
-    if (arrivalMinute>= minutesInHour){
-       arrivalMinute = arrivalMinute % minutesInHour
-        arrivalHour++
-    }
-    print(arrivalHour)
-    print(':')
-    print(arrivalMinute)
+    println("%02d:%02d".format(arrivalHour, arrivalMinute))
 }
