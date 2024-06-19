d6a6f3aa3b00a0bf863430e721643361768bf7e0
Arthur <dypis123@gmail.com>
2024-06-19 12:34:55 +0400

    KS-4-1 Train rework

--- src/main/kotlin/lesson4/lesson4_task1.kt
+++ src/main/kotlin/lesson4/lesson4_task1.kt
@@ -1,23 +1,24 @@
 package org.example.lesson4
-const val countTable = 13
 
-fun main(){
+const val Count_Table = 13
+
+fun main() {
     val todayFreeTables = 0
     val tomorrowFreeTables = 4
     var freeTables: Boolean
 
-    if (tomorrowFreeTables==0){
+    if (tomorrowFreeTables == 0) {
         freeTables = false
         println("Доступность столиков на завтра: $freeTables")
-    } else{
+    } else {
         freeTables = true
         println("Доступность столиков на завтра: $freeTables")
     }
 
-    if (todayFreeTables == 0){
+    if (todayFreeTables == 0) {
         freeTables = false
         println("Доступность столиков на сегодня: $freeTables")
-    } else{
+    } else {
         freeTables = true
         println("Доступность столиков на сегодня: $freeTables")
     }
